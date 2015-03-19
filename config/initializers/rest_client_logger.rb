185fe5d27c66346d9f05b6a3913aa4a7b7e77a4b
Dustin McCraw <dustin@lightspeedsystems.com>
2015-03-19 11:30:14 -0700

    rest logger

---
+++ config/initializers/rest_client_logger.rb
@@ -0,0 +1,10 @@
+# RestClient logs using << which isn't supported by the Rails logger,
+# so wrap it up with a little proxy object.
+unless Rails.env.production?
+  RestClient.log =
+    Object.new.tap do |proxy|
+      def proxy.<<(message)
+        Rails.logger.info message
+      end
+    end
+end
